f3c38139ccf5346a62385ac5e19078ef118e0ddd
Jon Lake <jlake21@ford.com>
2022-04-20 16:00:12 -0600

    Remove var instances

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-var currentId = 1;
+let currentId = 1;
 export function createGame(player1Name, player2Name) {
-  var player1 = {}
+  const player1 = {}
   player1.name = player1Name
   player1.score = 0
-  var player2 = {}
+  const player2 = {}
   player2.name = player2Name
   player2.score = 0
 
-  var game = {
+  const game = {
     id: currentId++,
     isComplete: false,
     players: [],
@@ -27,36 +27,37 @@ export function createGame(player1Name, player2Name) {
 }
 
 export function getScore(game) {
-  var player1 = game.players[0]
-  var player2 = game.players[1]
-  var player1Score = game.players[0].score
-  var player2Score = game.players[1].score
+  const player1 = game.players[0]
+  const player2 = game.players[1]
+  const player1Score = game.players[0].score
+  const player2Score = game.players[1].score
+  let score = ""
 
   if (player1Score === player2Score) {
     switch (player1Score) {
       case 0:
-        var score = "Love-All";
+        score = "Love-All";
         break;
       case 1:
-        var score = "Fifteen-All";
+        score = "Fifteen-All";
         break;
       case 2:
-        var score = "Thirty-All";
+        score = "Thirty-All";
         break;
       default:
-        var score = "Deuce";
+        score = "Deuce";
         break;
     }
   } else if (player1Score >= 4 || player2Score >= 4) {
-    var minusResult = player1Score - player2Score
-    if (minusResult === 1) var score = "Advantage player1";
-    else if (minusResult === -1) var score = "Advantage player2";
-    else if (minusResult >= 2) var score = "Win for " + player1.name
-    else var score = "Win for " + player2.name
+    const minusResult = player1Score - player2Score
+    if (minusResult === 1) score = "Advantage player1";
+    else if (minusResult === -1) score = "Advantage player2";
+    else if (minusResult >= 2) score = "Win for " + player1.name
+    else score = "Win for " + player2.name
   } else {
-    var tempScore
-    var score = ""
-    for (var i = 1; i < 3; i++) {
+    let tempScore
+    score = ""
+    for (let i = 1; i < 3; i++) {
       if (i === 1) tempScore = player1Score
       else {
         score += "-";
